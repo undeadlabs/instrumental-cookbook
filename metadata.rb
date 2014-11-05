1e7fa40512b34969e0e12ccb1ab388626102eee1
Jamie Winsor <jamie@vialstudios.com>
2014-11-04 16:59:49 -0800

    version bump 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "jamie@undeadlabs.com"
 license          "MIT"
 description      "Installs and configures components of instrumental (instrumentalapp.com)"
 long_description "Installs and configures components of instrumental (instrumentalapp.com)"
-version          "0.1.0"
+version          "0.1.1"
 
 supports "ubuntu"
 
